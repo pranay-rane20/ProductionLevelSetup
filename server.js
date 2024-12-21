2a3835ad2e6f0e42ed9ea3a1df0fa04af988212f
Pranay Rane <pranayrane029@gmail.com>
2024-12-21 15:30:29 +0530

    Second

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = require('./src/app');
 
 const server = http.createServer(app);
 
+
 server.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
